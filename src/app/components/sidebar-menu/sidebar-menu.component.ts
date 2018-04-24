24a65d18f22c4f790b4d08af2c3a2bf12cd05065
seesaw game <tttjjjccc@gmail.com>
2018-04-24 16:56:17 +0800

    Update SidebarMenu.

--- src/app/components/sidebar-menu/sidebar-menu.component.ts
+++ src/app/components/sidebar-menu/sidebar-menu.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit, Input, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 import { SidebarMenu } from '../../entities/sidebar-menu';
 import { CollapseNextDirective } from '../../directives/collapse-next/collapse-next.directive';
 
+const MENU_ID_PREFIX = 'menu-';
 const LEVEL_STYLE_PREFIX = 'level-';
 const SELECTED_STYLE = 'selected';
+const ACTIVED_STYLE = 'active';
 
 @Component({
   selector: 'app-sidebar-menu',
@@ -13,37 +16,78 @@ const SELECTED_STYLE = 'selected';
 export class SidebarMenuComponent implements OnInit {
 
   @Input() menuData: SidebarMenu;
-  menuStyle: string = '';
+  menuId: string = '';
+  menuStyle: string[] = [];
   nextLevelMenuGroupStyle: string = 'collapse';
-  iconStyle: string[] = ['fas', 'angle-down'];
+  menuIconStyle: string[] = [];
+  navIconStyle: string[] = [];
   isSelected: boolean = false;
+  isActive: boolean = false;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
+    this.setMenuId();
+    this.setMenuIcon();
+    this.setNavIcon(this.isSelected);
     this.setMenuStyle(this.isSelected);
     this.setChildMenuGroupStyle(this.isSelected);
   }
 
-  setMenuStyle(isSelected: boolean) {
-    const levelStyle = this.getLevelClass(this.menuData.level);
-    this.menuStyle = isSelected ? levelStyle + ' ' + SELECTED_STYLE : levelStyle;
-    this.iconStyle = isSelected ? ['fas', 'angle-up'] : ['fas', 'angle-down'];
-  }
-
-  setChildMenuGroupStyle(isShown: boolean) {
-    this.nextLevelMenuGroupStyle = isShown ? 'expand' : 'collapse';
-  }
-
-  getLevelClass(level: number) {
+  getLevelClass(level: number): string {
     return LEVEL_STYLE_PREFIX + level.toString();
   }
 
+  setMenuId() {
+    this.menuId = MENU_ID_PREFIX + this.menuData.routerLink;
+  }
+
+  setMenuStyle(isActive: boolean) {
+    this.menuStyle = isActive ? [this.getLevelClass(this.menuData.level), 'active'] : [this.getLevelClass(this.menuData.level)];
+  }
+
+  setMenuIcon() {
+    this.menuIconStyle = ['fas', this.menuData.icon];
+  }
+
+  setNavIcon(isNextLevelExpanded: boolean) {
+    if (this.menuData.nextLevel.length === 0) {
+      this.navIconStyle = [];
+    } else {
+      this.navIconStyle = isNextLevelExpanded ? ['fas', 'angle-up'] : ['fas', 'angle-down'];
+    }
+  }
+
+  setChildMenuGroupStyle(isShown: boolean) {
+    if (this.menuData.nextLevel.length !== 0) {
+      this.nextLevelMenuGroupStyle = isShown ? 'expand' : 'collapse';
+    }
+  }
+
+  setMenuSelected(isSelected: boolean) {
+    this.setChildMenuGroupStyle(isSelected);
+  }
+
+  setMenuActive(isActive: boolean) {
+    this.isActive = isActive;
+    this.setMenuStyle(this.isActive);
+  }
+
   selectMenu() {
-    this.isSelected = this.isSelected === true ? false : true;
-    this.setMenuStyle(this.isSelected);
+
+    this.isSelected = !this.isSelected;
+    this.setNavIcon(this.isSelected);
     this.setChildMenuGroupStyle(this.isSelected);
 
+    if (this.menuData.routerLink !== '') {
+      this.navigateRoute();
+      this.setMenuActive(this.isSelected);
+    }
+
+  }
+
+  navigateRoute() {
+    this.router.navigate([this.menuData.routerLink]);
   }
 
 }
